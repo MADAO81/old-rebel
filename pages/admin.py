08871df8cdf7994496cca11dca840a9fd2abd6df
Black Dow <chenyshev.evgeniy@gmail.com>
2026-09-03 21:11:28 +0300

    Fix models import, remove duplicate Bike registration in admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Bike, ComparisonArticle, ContactMessage
 
-@admin.register(Bike)
-class BikeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'years')
-    prepopulated_fields = {'slug': ('name',)}
+# @admin.register(Bike)
+# class BikeAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'code', 'years')
+#     prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(ComparisonArticle)
 class ComparisonArticleAdmin(admin.ModelAdmin):
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib import messages
-from .models import Bike, ComparisonArticle, ContactMessage
+from .models import  ComparisonArticle, ContactMessage
+from bikes.models import Bike
 
 
 def index(request):
